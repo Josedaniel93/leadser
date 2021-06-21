d2b14a7e27d2c7ec5742654a4179816573f3a601
Jose Daniel <josedaniel@rethinkmarketing.es>
2021-06-21 21:45:33 +0200

    FixesTo Build pro

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:3000',
+  API_URL: 'http://leadser-back.herokuapp.com/',
   URL: {
     CREATEWORKER: '/workers/create',
     CREATEBUSINESS: '/business/create',
